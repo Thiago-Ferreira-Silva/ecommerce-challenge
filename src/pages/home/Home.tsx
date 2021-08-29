397bbb1870b74fcaf1b386e5c9ffb220f81042e1
Thiago-Ferreira-Silva <62727996+Thiago-Ferreira-Silva@users.noreply.github.com>
2021-08-29 11:46:48 -0300

    add Order by functionality

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,12 +7,45 @@ type Products = ReactNode[]
 
 export function Home() {
 
+    const orderOptions = ['nome', 'preço', 'score']
+
     const [products, setProducts] = useState([] as Products)
+    const [orderIndex, setOrderIndex] = useState(0)
 
     useEffect(() => {
+
+        function getOrderRule() {
+            switch(orderIndex) {
+                case 0:
+                    return 'name'
+                case 1:
+                    return 'price'
+                case 2:
+                    return 'score'
+                default:
+                    return 'name'
+            }
+        }
+
+        const orderRule = getOrderRule()
+
+        productsJSON.sort((a, b) => {
+            if (a[orderRule] > b[orderRule]) {
+                return 1
+            } else if (a[orderRule] < b[orderRule]) {
+                return -1
+            } else {
+                return 0
+            }
+        })
+
         const productsJSX = productsJSON.map(product => <Product {...product} key={product.id} />)
         setProducts(productsJSX)
-    }, [])
+    }, [orderIndex])
+
+    function handleSort() {
+        orderIndex < 2 ? setOrderIndex(orderIndex + 1) : setOrderIndex(0)
+    }
 
     return (
         <div className="home-container">
@@ -30,9 +63,9 @@ export function Home() {
                 </div>
             </header>
             <div className="order-by">
-                <button>
+                <button onClick={handleSort}>
                     <img src="/assets/arrow-down-icon.svg" alt="Ordenar" />
-                    <span>Ordenar por {}</span>
+                    <span>Ordenar por {orderOptions[orderIndex]}</span>
                 </button>
             </div>
             <section className="products">
