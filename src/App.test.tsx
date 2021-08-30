ee8d8d12aab7b5b0f5d7b491dec2cb09eb43a1ba
Thiago-Ferreira-Silva <62727996+Thiago-Ferreira-Silva@users.noreply.github.com>
2021-08-30 13:27:21 -0300

    add button functionality test to Product component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import { MemoryRouter } from 'react-router'
+import App from './App'
 
-test('renders learn react link', () => {
-  //render(<App />);
-  //const linkElement = screen.getByText(/learn react/i);
-  //expect(linkElement).toBeInTheDocument();
-});
+describe('Home', () => {
+
+    let container = document.createElement('div')
+    beforeEach(() => {
+        document.body.appendChild(container)
+    })
+
+    afterEach(() => {
+        unmountComponentAtNode(container)
+        container.remove()
+    })
+
+    it('should render', () => {
+        act(() => {
+            render(
+                <MemoryRouter>
+                    <App />
+                </MemoryRouter>,
+                container
+            )
+        })
+        
+        expect(container.querySelector('.App')?.children.length).toBe(2)
+    })
+  })
--- src/components/product/Product.test.tsx
+++ src/components/product/Product.test.tsx
@@ -1,6 +1,8 @@
+import { fireEvent } from '@testing-library/react'
 import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 import { act } from 'react-dom/test-utils'
+import { CartContext } from '../../contexts/CartContext'
 import { Product } from './Product'
 
 describe('Product', () => {
@@ -23,6 +25,29 @@ describe('Product', () => {
         image: "super-mario-odyssey.png"
     }
 
+    const cartItems = [{
+        id: 312,
+        name: "Super Mario Odyssey",
+        price: 197.88,
+        image: "super-mario-odyssey.png"
+    },
+    {
+        id: 201,
+        name: "Call Of Duty Infinite Warfare",
+        price: 49.99,
+        image: "call-of-duty-infinite-warfare.png"
+    },
+    {
+        id: 102,
+        name: "The Witcher III Wild Hunt",
+        price: 119.5,
+        image: "the-witcher-iii-wild-hunt.png"
+    }]
+
+    const removeAll = jest.fn()
+    const removeItem = jest.fn()
+    const addItem = jest.fn()
+
     it('should render the data from props', () => {
         act(() => {
             render(
@@ -36,11 +61,31 @@ describe('Product', () => {
         expect(container.querySelector('.price')?.textContent)
             .toBe(productData.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }))
         expect(container.querySelector('.score')?.textContent)
-            .toBe(productData.score.toString())
+            .toContain(productData.score.toString())
         expect(container.querySelector('img'))
             .toBeInTheDocument()
         expect(container.querySelector('button'))
             .toBeInTheDocument()
 
+    })
+
+    it('should add to cart', () => {
+
+        act(() => {
+            render(
+                <CartContext.Provider value={{ removeAll, removeItem, addItem, cartItems }}>
+                    <Product {...productData} />
+                </CartContext.Provider>,
+                container,
+            )
+        })
+
+        const button = container.querySelector('button') || document.createElement('button')
+
+        act(() => {
+            fireEvent.click(button)
+        })
+
+        expect(addItem).toHaveBeenCalledTimes(1)
     })
 })
